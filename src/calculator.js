8ba7b0ed96a4036319d5c53029821c132beb5440
harisaianubolu253 <harisaianubolu253@gmail.com>
2026-05-22 10:56:50 -0400

    Update calculator.js
    updated code to fix multiply function and removed the string declaration in divide function

--- src/calculator.js
+++ src/calculator.js
@@ -9,12 +9,12 @@ function subtract(a, b) {
 
 function multiply(a, b) {
   // Intentional practice bug: this adds instead of multiplying.
-  return a + b;
+  return a * b;
 }
 
 function divide(a, b) {
   // Intentional practice bug: this checks the string "0" instead of number 0.
-  if (b === "0") {
+  if (b === 0) {
     throw new Error("Cannot divide by zero");
   }
 
